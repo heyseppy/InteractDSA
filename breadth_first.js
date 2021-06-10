d6b1b07174ee88eeb6f0d4bb16f306571f062935
heyseppy <sepkimiaei1@gmail.com>
2021-06-10 22:41:03 +0800

    removed redundant peek function for my queue

--- breadth_first.js
+++ breadth_first.js
@@ -25,10 +25,6 @@
     			return this.elements.length == 0;
 			};
 
-			Queue.prototype.peek = function () 
-			{
-				return !this.isEmpty() ? this.elements[0] : undefined;
-			};
 			
 
 			let q = new Queue();
