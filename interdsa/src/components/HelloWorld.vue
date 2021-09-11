ef7b9e57c2a18c432f15a6584a714bc1836b0e69
Sep Kimiaei <62089596+heyseppy@users.noreply.github.com>
2021-09-11 18:29:36 +0800

    added some more documentation

--- interdsa/src/components/HelloWorld.vue
+++ interdsa/src/components/HelloWorld.vue
@@ -20,7 +20,7 @@
       background-color="grey lighten-2"
       color="cyan"
       label="adjmatrix"
-      v-model="aa"
+      v-model="userGraph"
     ></v-textarea>
       
       
@@ -36,7 +36,7 @@ import rough from 'roughjs/bundled/rough.cjs';
     
     data () {
       return {
-        aa: "",
+        userGraph: "",
 
       };
     },
@@ -45,83 +45,83 @@ import rough from 'roughjs/bundled/rough.cjs';
 
   drawGrid()
   {
+  
 
-    class Queue {
-  constructor() {
-    this.items = {};
-    this.headIndex = 0;
-    this.tailIndex = 0;
+  //Queue class for BFS functionality of drawing
+  class Queue {
+    constructor() {
+      this.items = {};
+      this.headIndex = 0;
+      this.tailIndex = 0;
+    }
+
+    enqueue(item) {
+      this.items[this.tailIndex] = item;
+      this.tailIndex++;
+    }
+
+    dequeue() {
+      const item = this.items[this.headIndex];
+      delete this.items[this.headIndex];
+      this.headIndex++;
+      return item;
+    }
+
+    peek() {
+      return this.items[this.headIndex];
+    }
+
+    get length() {
+      return this.tailIndex - this.headIndex;
+    }
   }
 
-  enqueue(item) {
-    this.items[this.tailIndex] = item;
-    this.tailIndex++;
-  }
 
-  dequeue() {
-    const item = this.items[this.headIndex];
-    delete this.items[this.headIndex];
-    this.headIndex++;
-    return item;
-  }
-
-  peek() {
-    return this.items[this.headIndex];
-  }
-
-  get length() {
-    return this.tailIndex - this.headIndex;
-  }
-}
-
-
+    //convert user given adjmatrix into a 2D array using regex
     let adjmatrix = [];
-    var tmp = (this.aa).split('], ');
+    var tmp = (this.userGraph).split('], ');
 
     for (var i=0; i<tmp.length; i++) {
         adjmatrix.push(tmp[i].replace(/\[|\]/g, '').split(', '));
     }
 
-    //adjmatrix[0][0] = 0;
+    //print contents of the adjmatrix
     console.log(adjmatrix);
 
     let start_off_hang = 800;
     const drawingQ = new Queue();
     let visited = [];
     let drawn = [];
-    let coords = [
-      [start_off_hang,110],
-      [0,0],
-      [0,0],
-      [0,0],
-      [0,0],
-      [0,0],
-      [0,0],
-      [0,0],
-      [0,0],
-      [0,0],
-    ];
+    let coords = [];
 
+    //create a coordinate array
+    for (var i=0; i<adjmatrix.length; i++)
+    {
+      coords.push([0,0]);
+    }
+
+    //set root node position
+    coords[0][0] = start_off_hang;
+    coords[0][1] = 110;
     drawingQ.enqueue(0);
-
     const rc = rough.canvas(document.getElementById('canvas'));
     rc.circle(coords[0][0], coords[0][1], 40, { fill: 'red' });
 
+
+
+    //perform a BFS drawing each node with a line connect their.X, theirY to the relavant parent.X and parentY
     while (drawingQ.length != 0)
     {
       
       var currentNode = drawingQ.dequeue();
-      //rc.circle(coords[currentNode][0], coords[currentNode][1], 40, { fill: 'red' });
       console.log("... exploring from node: " + currentNode);
       visited.push(currentNode);
       
-      
       let number_of_children = 4;
-      console.log(number_of_children);
-      let cnt = -0.5 * number_of_children;
+      let cnt = - 0.5 * number_of_children;
       let offset =  100;
 
-      for (let i = 0; i < adjmatrix[currentNode].length; i++)
+      for (var i = 0; i < adjmatrix[currentNode].length; i++)
       {
         if (adjmatrix[currentNode][i] == 1 && !visited.includes(i) && currentNode != i)
         {
